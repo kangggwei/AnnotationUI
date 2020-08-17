2586fcced16541c09ca54e459ce54889dea62f2d
kangggwei <ong.kangwei@dhs.sg>
2020-08-17 22:35:46 +0800

    updated annotations

--- annotations/src/components/Annotate.js
+++ annotations/src/components/Annotate.js
@@ -5,57 +5,67 @@ import NavBar from "./Navbar";
 const Annotate = () => {
   const queryString = window.location.pathname.replace("/", "");
   const part = queryString.replace(/[0-9]/g, "");
-  const image = { filename: queryString + ".jpg" };
+  var image = { filename: queryString + ".jpg" };
 
   const [select, setSelect] = useState("valid");
 
-  console.log(select);
+  const changeOnClick = (e) => {
+    e.preventDefault();
+
+    image.status = select;
+    console.log(image);
+  };
 
   return (
     <>
       <NavBar></NavBar>
       <AnnotateContainer>
-        <div className="container">
-          <h4>Filename: {image.filename}</h4>
-          <br />
-          <img
-            src={require(`../output/${part}/${image.filename}`)}
-            alt="validation"
-          ></img>
-          <hr />
-        </div>
-        <div className="btn-group btn-group-toggle" data-toggle="buttons">
-          <label className="btn btn-primary active">
-            <input
-              type="radio"
-              name="options"
-              value="valid"
-              checked={select === "valid"}
-              onChange={(e) => setSelect(e.target.value)}
-            />{" "}
-            Valid
-          </label>
-          <label className="btn btn-danger">
-            <input
-              type="radio"
-              name="options"
-              value="invalid"
-              checked={select === "invalid"}
-              onChange={(e) => setSelect(e.target.value)}
-            />{" "}
-            Invalid
-          </label>
-          <label className="btn btn-secondary">
-            <input
-              type="radio"
-              name="options"
-              value="unusable"
-              checked={select === "unusable"}
-              onChange={(e) => setSelect(e.target.value)}
-            />{" "}
-            Unusable
-          </label>
-        </div>
+        <form onSubmit={changeOnClick} encType="multipart/form-data">
+          <div className="container">
+            <h4>Filename: {image.filename}</h4>
+            <br />
+            <img
+              src={require(`../output/${part}/${image.filename}`)}
+              alt="validation"
+            ></img>
+            <hr />
+          </div>
+          <div className="btn-group btn-group-toggle" data-toggle="buttons">
+            <label className="btn btn-outline-primary">
+              <input
+                type="radio"
+                name="options"
+                value="valid"
+                checked={select === "valid"}
+                onChange={(e) => setSelect(e.target.value)}
+              />{" "}
+              Valid
+            </label>
+            <label className="btn btn-outline-danger">
+              <input
+                type="radio"
+                name="options"
+                value="invalid"
+                checked={select === "invalid"}
+                onChange={(e) => setSelect(e.target.value)}
+              />{" "}
+              Invalid
+            </label>
+            <label className="btn btn-outline-secondary">
+              <input
+                type="radio"
+                name="options"
+                value="unusable"
+                checked={select === "unusable"}
+                onChange={(e) => setSelect(e.target.value)}
+              />{" "}
+              Unusable
+            </label>
+            <button type="submit" className="btn btn-primary">
+              Next
+            </button>
+          </div>
+        </form>
       </AnnotateContainer>
     </>
   );
